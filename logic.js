24b71a6feb9e374025fa56a55827e757f9e51eb3
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2021-08-26 17:32:00 +0100

    fixed the god damn size

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 var particles = document.getElementById('particles');
 window.addEventListener('scroll', function(){
     var scrolling = window.scrollY;
-    particles.style.top = (scrolling + 40)* 0.5 + 'px';
+    particles.style.top = (scrolling + 300)* 0.5 + 'px';
     
-    particles.style.transform = "rotate(-"+scrolling * 0.05+"deg)";
+    particles.style.transform = "rotate(-"+scrolling * 0.01+"deg)";
 });
