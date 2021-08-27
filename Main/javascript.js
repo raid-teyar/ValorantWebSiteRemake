b279dd1bda1485768d60617f7d81d972569fa140
RomeoDaBlackRose <baiche.ramii@gmail.com>
2021-08-27 17:36:15 +0100

    Updated & added files
    Updated some css files and added the Main page file.

---
+++ Main/javascript.js
@@ -0,0 +1,15 @@
+var particles = document.getElementById('particles');
+window.addEventListener('scroll', function() {
+    var scrolling = window.scrollY;
+    particles.style.top = (scrolling + 300) * 0.5 + 'px';
+});
+
+$(window).scroll(function() {
+    if ($(window).scrollTop()) {
+        $("header").addClass("black");
+        $("a").addClass("black");
+    } else {
+        $("header").removeClass("black");
+        $("a").removeClass("black");
+    }
+});
