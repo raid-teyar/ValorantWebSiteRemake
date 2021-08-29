b0883938da2b1951346cd5ab4bba4cbba889e812
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2021-08-29 10:08:14 +0100

    added logo animation

--- landing/logic.js
+++ landing/logic.js
@@ -1,16 +1,19 @@
-var particles = document.getElementById('particles');
-window.addEventListener('scroll', function(){
+$(window).on("scroll", function(){
     var scrolling = window.scrollY;
-    particles.style.top = (scrolling + 300)* 0.5 + 'px';
+    $(".home-pattern").css("top", (scrolling + 300)* 0.5 + 'px');
 });
 
-$(window).scroll(function() {
+$(window).on("scroll",function() {
 	if ($(window).scrollTop()) {
 		$("header").addClass("black");
-    $("a").addClass("black");
+    	$("a").addClass("black");
+		$(".logo").css("visibility","collapse")
+		$("header").css("padding","10px 29% 10px 25%");
 		}
 	else{
 		$("header").removeClass("black");
-    $("a").removeClass("black");
+    	$("a").removeClass("black");
+		$(".logo").css("visibility","visible")
+		$("header").css("padding","10px 300px");
 		}
 	});
