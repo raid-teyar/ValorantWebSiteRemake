edcadb2510cee6337bd97e186a617cb7a550c0a2
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2021-09-11 11:14:10 +0100

    added vertical NavBar and Go to start Button

--- Main/javascript.js
+++ Main/javascript.js
@@ -1,7 +1,13 @@
 var particles = document.getElementById('particles');
+var goToStartBtn = document.getElementById('goToStartButton');
 window.addEventListener('scroll', function() {
     var scrolling = window.scrollY;
     particles.style.top = (scrolling + 300) * 0.5 + 'px';
+    if(this.scrollY == 0 || this.scrollY < 300){
+        goToStartBtn.style.visibility = 'collapse';
+    }else{
+        goToStartBtn.style.visibility = 'visible';
+    }
 });
 
 var progerssbar = document.getElementById('progressbar');
@@ -15,7 +21,30 @@ $(window).scroll(function() {
         $("header").removeClass("black");
         $("a").removeClass("black");
     }
-    var progresshight = (window.pageYOffset / totalhight) * 78;
+    var progresshight = (window.pageYOffset / totalhight) * 100;
     progerssbar.style.height = progresshight + "%";
 });
+
+var navbtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+var header = document.getElementById('header');
+var navitems = document.getElementById('navitems');
+navbtn.addEventListener('click', function () {
+    if (!menuOpen) {
+        header.style.width = '25%'
+        navitems.style.visibility = 'visible';
+        navbtn.style.position = 'fixed';
+        navbtn.classList.add('open');
+        menuOpen = true;
+    }else{
+        header.style.width = '0%';
+        navitems.style.visibility = 'collapse';
+        navbtn.style.position = 'absolute';
+        navbtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+
+
 
