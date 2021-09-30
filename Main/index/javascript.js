926134eaa91945c4df60366232be50697ce1ebe6
RomeoDaBlackRose <baiche.ramii@gmail.com>
2021-09-30 21:13:52 +0100

    updated some stuff
    uh

--- Main/index/javascript.js
+++ Main/index/javascript.js
@@ -6,9 +6,9 @@ window.onload = function() {
     $loader.classList.remove('loader--active')
     progerssbar.style.height = 0;
     $(window).scroll(function() {
-    var progresshight = (window.pageYOffset / totalhight) * 100;
-    progerssbar.style.height = progresshight + "%";
-});
+        var progresshight = (window.pageYOffset / totalhight) * 100;
+        progerssbar.style.height = progresshight + "%";
+    });
 };
 
 
@@ -18,16 +18,16 @@ window.onload = function() {
 var settingsBtn = document.getElementById('goToStartButton');
 var settingsMenu = document.getElementById('setings');
 var visible = false;
-settingsBtn.addEventListener("click", function () {
+settingsBtn.addEventListener("click", function() {
     if (visible) {
         settingsMenu.style.opacity = 0;
         settingsMenu.style.height = 0;
-        settingsBtn.style.transform  = 'rotate(80deg)';
+        settingsBtn.style.transform = 'rotate(80deg)';
         visible = false;
-    }else{
+    } else {
         settingsMenu.style.opacity = 1;
         settingsMenu.style.height = '200px';
-        settingsBtn.style.transform  = 'rotate(-80deg)';
+        settingsBtn.style.transform = 'rotate(-80deg)';
         visible = true;
     }
 });
@@ -37,16 +37,16 @@ var isBlack = true;
 var root = document.querySelector(':root');
 var rs = getComputedStyle(root);
 var themeBtn = document.getElementById('themeBtn');
-themeBtn.addEventListener('click',function(){
+themeBtn.addEventListener('click', function() {
     if (isBlack) {
-        document.documentElement.style.setProperty('--base-color','#ece8e1')
-        document.documentElement.style.setProperty('--white-color','#0f1923')
-        document.documentElement.style.setProperty('--secondbase-color','#ece8e1')
+        document.documentElement.style.setProperty('--base-color', '#ece8e1')
+        document.documentElement.style.setProperty('--white-color', '#0f1923')
+        document.documentElement.style.setProperty('--secondbase-color', '#ece8e1')
         isBlack = false;
-    }else{
-        document.documentElement.style.setProperty('--base-color','#0f1923')
-        document.documentElement.style.setProperty('--white-color','#ece8e1')
-        document.documentElement.style.setProperty('--secondbase-color','#101822')
+    } else {
+        document.documentElement.style.setProperty('--base-color', '#0f1923')
+        document.documentElement.style.setProperty('--white-color', '#ece8e1')
+        document.documentElement.style.setProperty('--secondbase-color', '#101822')
         isBlack = true;
     }
 });
@@ -57,11 +57,11 @@ var musicOn = true;
 var music = document.getElementById('audio');
 var musicBtn = document.getElementById('musicBtn');
 music.volume = '0.5';
-musicBtn.addEventListener('click', function () {
+musicBtn.addEventListener('click', function() {
     if (musicOn) {
         music.volume = 0;
         musicOn = false;
-    }else{
+    } else {
         music.volume = '0.5';
         musicOn = true;
     }
@@ -98,3 +98,14 @@ navbtn.addEventListener('click', function() {
         menuOpen = false;
     }
 });
+
+
+/* RANDOM VIDEOS */
+var random = Math.floor(Math.random() * $('.animation').length);
+$('.animation').hide().eq(random).show();
+
+var randomm = Math.floor(Math.random() * $('.animationn').length);
+$('.animationn').hide().eq(randomm).show();
+
+var randommm = Math.floor(Math.random() * $('.animationnn').length);
+$('.animationnn').hide().eq(randommm).show();
