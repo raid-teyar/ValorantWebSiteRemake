1725420df26ea689db6f0e9e0357f7f6204c55d6
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2021-10-01 00:16:57 +0100

    last commit i guess

--- Main/agents/cursor.js
+++ Main/agents/cursor.js
@@ -1,60 +1,57 @@
-document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-    t.style.left = n.clientX + "px", 
-    t.style.top = n.clientY + "px", 
-    e.style.left = n.clientX + "px", 
-    e.style.top = n.clientY + "px", 
-    i.style.left = n.clientX + "px", 
-    i.style.top = n.clientY + "px"
-});
-var t = document.getElementById("cursor"),
-    e = document.getElementById("cursor2"),
-    i = document.getElementById("cursor3");
-function n(t) {
-    e.classList.add("hover"), i.classList.add("hover")
-}
-function s(t) {
-    e.classList.remove("hover"), i.classList.remove("hover")
-}
-s();
-for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
-    o(r[a])
-}
-function o(t) {
-    t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-}
 
-$(document).ready(function() {
-    
-    /* Hero Case study images */			
-    
-    $('.case-study-name:nth-child(1)').on('mouseenter', function() {
-        $('.case-study-name.active').removeClass('active');
-        $('.case-study-images li.show').removeClass("show");
-        $('.case-study-images li:nth-child(1)').addClass("show");
-        $('.case-study-name:nth-child(1)').addClass('active');
-    })
-    $('.case-study-name:nth-child(2)').on('mouseenter', function() {
-        $('.case-study-name.active').removeClass('active');
-        $('.case-study-images li.show').removeClass("show");
-        $('.case-study-images li:nth-child(2)').addClass("show");
-        $('.case-study-name:nth-child(2)').addClass('active');
-    })
-    $('.case-study-name:nth-child(3)').on('mouseenter', function() {
-        $('.case-study-name.active').removeClass('active');
-        $('.case-study-images li.show').removeClass("show");
-        $('.case-study-images li:nth-child(3)').addClass("show");
-        $('.case-study-name:nth-child(3)').addClass('active');
-    })
-    $('.case-study-name:nth-child(4)').on('mouseenter', function() {
-        $('.case-study-name.active').removeClass('active');
-        $('.case-study-images li.show').removeClass("show");
-        $('.case-study-images li:nth-child(4)').addClass("show");
-        $('.case-study-name:nth-child(4)').addClass('active');
-    })
-    $('.case-study-name:nth-child(1)').trigger('mouseenter')
-                
+/* ##### settings ##### */
+
+var settingsBtn = document.getElementById('goToStartButton');
+var settingsMenu = document.getElementById('setings');
+var visible = false;
+settingsBtn.addEventListener("click", function () {
+    if (visible) {
+        settingsMenu.style.opacity = 0;
+        settingsMenu.style.height = 0;
+        settingsBtn.style.transform  = 'rotate(80deg)';
+        visible = false;
+    }else{
+        settingsMenu.style.opacity = 1;
+        settingsMenu.style.height = '200px';
+        settingsBtn.style.transform  = 'rotate(-80deg)';
+        visible = true;
+    }
 });
 
+/* theme */
+var isBlack = true;
+var root = document.querySelector(':root');
+var rs = getComputedStyle(root);
+var themeBtn = document.getElementById('themeBtn');
+themeBtn.addEventListener('click',function(){
+    if (isBlack) {
+        document.documentElement.style.setProperty('--base-color','#ece8e1')
+        document.documentElement.style.setProperty('--white-color','#0f1923')
+        document.documentElement.style.setProperty('--secondbase-color','#ece8e1')
+        isBlack = false;
+    }else{
+        document.documentElement.style.setProperty('--base-color','#0f1923')
+        document.documentElement.style.setProperty('--white-color','#ece8e1')
+        document.documentElement.style.setProperty('--secondbase-color','#101822')
+        isBlack = true;
+    }
+});
+
+
+/* audio */
+var musicOn = true;
+var music = document.getElementById('audio');
+var musicBtn = document.getElementById('musicBtn');
+music.volume = '0.5';
+musicBtn.addEventListener('click', function () {
+    if (musicOn) {
+        music.volume = 0;
+        musicOn = false;
+    }else{
+        music.volume = '0.5';
+        musicOn = true;
+    }
+});
 
 
 
